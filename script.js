cfc6d2e623dbd6e59e3b29e0a53d731db726d689
Miaplacidus14 <colombangh@gmail.com>
2023-07-21 19:45:08 +0200

    Amélioration du jeu

--- script.js
+++ script.js
@@ -1,60 +1,61 @@
+const container = document.querySelector('.results');
+const results = document.querySelector
+const paragraph = document.createElement('p');
+const computerChoice = document.createElement('p')
+container.appendChild(computerChoice);
+container.appendChild(paragraph);
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice () {
     number = Math.floor(Math.random() * 3);
     if (number === 0) {
-        console.log("Computer choice's : rock");
+        computerChoice.textContent = "Computer choice : rock";
         return 'rock';
     } else if (number === 1) {
-        console.log("Computer choice's : paper");
+        computerChoice.textContent = "Computer choice : paper";
         return 'paper';
     } else {
-        console.log("Computer choice's : scissors");
+        computerChoice.textContent = "Computer choice : scissors";
         return 'scissors';
     }
 
 }
 
 function playRound (playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
     if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return 'You Win! Rock beats Scissors';
+        paragraph.textContent =  'You Win! Rock beats Scissors';
+        document.querySelector('.player-score').textContent = `Player : ${++playerScore}`;
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return 'You Win! Paper beats Rock';
+        paragraph.textContent = 'You Win! Paper beats Rock';
+        document.querySelector('.player-score').textContent = `Player : ${++playerScore}`;
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return 'You Win! Scissors beats Paper';
+        paragraph.textContent = 'You Win! Scissors beats Paper';
+        document.querySelector('.player-score').textContent = `Player : ${++playerScore}`;
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return 'You Lose! Rock beats Scissors';
+        paragraph.textContent = 'You Lose! Rock beats Scissors';
+        document.querySelector('.computer-score').textContent = `Computer : ${++computerScore}`;
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return 'You Lose! Paper beats Rock';
+        paragraph.textContent = 'You Lose! Paper beats Rock';
+        document.querySelector('.computer-score').textContent = `Computer : ${++computerScore}`;
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return 'You Lose! Scissors beats Paper';
+        paragraph.textContent = 'You Lose! Scissors beats Paper';
+        document.querySelector('.computer-score').textContent = `Computer : ${++computerScore}`;
     } else {
-        return "Equal !"
+        paragraph.textContent = "Equal !";
+    }
+    if (computerScore === 5) {
+        paragraph.textContent = 'Stop !, You Lose, Computer haves 5 points';
+        computerScore = 0
+        playerScore = 0
+        document.querySelector('.computer-score').textContent = `Computer : ${computerScore}`;
+        document.querySelector('.player-score').textContent = `Player : ${playerScore}`;
+    } else if (playerScore === 5) {
+        paragraph.textContent = 'Stop !, You Win, You have 5 points';
+        computerScore = 0
+        playerScore = 0
+        document.querySelector('.computer-score').textContent = `Computer : ${computerScore}`;
+        document.querySelector('.player-score').textContent = `Player : ${playerScore}`;
     }
 }
 
-function game () {
-    let playerScore = 0
-    let computerScore = 0
-    for (let i = 0; i < 5; i++) {
-        playerSelection = prompt("Please choose between rock, paper and scissors!")
-        let round = playRound(playerSelection, getComputerChoice());
-        console.log(round)
-        if (round.slice(0, 7) === 'You Win') {
-            playerScore++
-        } else if (round.slice(0, 8) === 'You Lose') {
-            computerScore++
-        } else {
-            computerScore++
-            playerScore++
-        }
-    }
-    if (computerScore > playerScore) {
-        return 'Computer Win!'
-    } else if (computerScore < playerScore) {
-        return 'You Win!'
-    } else {
-        return 'Equal!'
-    }
-}
-
-console.log(game())
